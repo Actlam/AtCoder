197553d57459ae2bacb994cbeff27ba217fcdf8d
Yuki Ogawa <stream.actlam@gmail.com>
2020-04-04 03:26:36 +0900

    rm test.py

---
+++ ABC157/C_Guess_The_Number.py
@@ -0,0 +1,15 @@
+N, M = list(map(int, input().split()))
+s = []
+c = []
+s_ = []
+c_ = [] 
+
+
+for _ in range(M):
+    s_, c_ = map(int, input().split())
+    s.append(s_)
+    c.append(c_)
+
+ans = []
+
+
